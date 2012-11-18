9c959ab82ff062db7ac5696dfc17d458bc6a2495
coopaloopster <pjc370@nyu.edu>
2012-11-18 16:17:16 -0500

    problem 3 complete

--- Prob_3.py
+++ Prob_3.py
@@ -1,23 +1,32 @@
 #  PJC  ###################################
 import numpy as np
-N = 100
+N = 600851475143
 maxi = int(np.sqrt(N))+1
-for i in range(2,maxi):
-  if (N%i==0):
+bfactors = []
+tfactors = []
+primes = [0]
+for i in range(1,maxi):
+    if (N%i == 0):
+        bfactors.append(i)
+        tfactors.append(N/i)
+for i in tfactors:
+    maxitest = int(np.sqrt(i)+1)
     count = 0
-    test = N/i
-    print count
-    print test
-    maxitest = int(np.sqrt(test))+1
     for j in range(2,maxitest):
-      if (test%j == 0):
-        count = 1
-        break
+        if (i%j == 0):
+            count = 1
+            break
     if (count == 0):
-        ans = test
-        break
-    print count
-print ans
+        primes.append(i)
+if (max(primes) == 0):
+    for i in bfactors:
+        maxitest = int(np.sqrt(i))+1
+        count = 0
+        for j in range(2,maxitest):
+            if (i%j == 0):
+                count = 1
+                break
+        if (count == 0):
+            primes.append(i)
+print max(primes)
 #############################################
-
-
